7c72156ec9533d30d63155cbc7d6de89191c6e4a
Marwane Tchassama <marwane.tchassama@enuygun.com>
2021-02-07 02:08:46 +0300

    css and js library requirements added to admin.js

--- assets/admin.js
+++ assets/admin.js
@@ -1,5 +1,18 @@
 const $ = require('jquery');
+
 require('bootstrap');
+
 require('bootstrap/js/dist/popover');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+require('summernote');
+require('icheck-bootstrap');
+require('overlayscrollbars');
+require('daterangepicker');
+require('jquery-ui');
+require('chart.js');
+require('moment');
 
 import './styles/adminlte.scss';
+
+
--- assets/app.js
+++ assets/app.js
@@ -21,7 +21,7 @@ require('bootstrap');
 
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
- require('bootstrap/js/dist/popover');
+require('bootstrap/js/dist/popover');
 
 // require the JavaScript
 require('bootstrap-star-rating');
