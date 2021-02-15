b4396815452135bddfc7486ac42aae7c9dfd0f7c
Marwane Tchassama <marwane.tchassama@enuygun.com>
2021-02-16 00:56:00 +0300

    boss implemented

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -14,7 +14,7 @@ class AdminController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('pages/status/en_attente_etude.html.twig');
+        return $this->render('pages/status/manager_analysis.html.twig');
     }
 
     /**
@@ -75,7 +75,6 @@ class AdminController extends AbstractController
         return $this->render('pages/status/to_be_reanalysed.html.twig');
     }
 
-
     /**
      * @Route("/manager_analysis", name="app-manager-view")
      * @return Response
@@ -85,6 +84,66 @@ class AdminController extends AbstractController
         return $this->render('pages/status/manager_analysis.html.twig');
     }
 
+    /**
+     * @Route("/manager_analysis-on", name="app-manager-on")
+     * @return Response
+     */
+    public function managerAnalysisOnGoing(): Response
+    {
+        return $this->render('pages/status/analysis_on_going.html.twig');
+    }
 
+    /**
+     * @Route("/manager_validated", name="app-manager-validated")
+     * @return Response
+     */
+    public function managerValidated(): Response
+    {
+        return $this->render('pages/status/been_validated.html.twig');
+    }
 
+    /**
+     * @Route("/manager_view_validated", name="app-manager-view-validated")
+     * @return Response
+     */
+    public function managerViewValidated(): Response
+    {
+        return $this->render('pages/statusRedirections/boss_view_for_validated.html.twig');
+    }
+
+    /**
+     * @Route("/manager_view_validated_financed", name="app-manager-view-validated")
+     * @return Response
+     */
+    public function managerViewValidatedFinanced(): Response
+    {
+        return $this->render('pages/status/validated_financed.html.twig');
+    }
+
+    /**
+     * @Route("/manager_view_validated_financed_view", name="app-manager-valiated-financed-view")
+     * @return Response
+     */
+    public function managerViewValidatedFinancedView(): Response
+    {
+        return $this->render('pages/statusRedirections/boss_view_for_validated_financed.html.twig');
+    }
+
+    /**
+     * @Route("/manager_view_lacking_fund", name="app-manager-lacking")
+     * @return Response
+     */
+    public function managerViewlacking(): Response
+    {
+        return $this->render('pages/status/lacking_fund.html.twig');
+    }
+
+    /**
+     * @Route("/manager_financial_report", name="app-manager-financial-report")
+     * @return Response
+     */
+    public function managerFinancialReport(): Response
+    {
+        return $this->render('tables/financial_report.html.twig');
+    }
 }
