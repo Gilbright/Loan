c7887fd7723da135b26394b2f2004c9d30e201f3
gildas <gildas.decadjevi@enuygun.com>
2021-02-20 16:25:01 +0300

    client resolver and names added to twig

---
+++ src/Service/OptionsResolver/ClientResolver.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Service\OptionsResolver;
+
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClientResolver extends AbstractOptionResolver
+{
+    public static function _resolve(array $client)
+    {
+        $client = (new OptionsResolver())
+            ->setRequired([
+                'nameSurname',
+                'birthDate',
+                'gender',
+                'nationality',
+                'phoneNumber',
+                'email',
+                'profession',
+                'monthlyIncome',
+                'address',
+                'idNumber',
+                'confirm'
+                ])
+            ->setAllowedTypes('nameSurname', 'string')
+            ->setAllowedTypes('birthDate', ['string', 'DateTime'])
+            ->setAllowedTypes('gender', ['string', 'int', 'bool'])
+            ->setAllowedTypes('nationality', 'string')
+            ->setAllowedTypes('phoneNumber',['string', 'int'] )
+            ->setAllowedTypes('email', 'string')
+            ->setAllowedTypes('profession', 'string')
+            ->setAllowedTypes('monthlyIncome', ['string', 'int'])
+            ->setAllowedTypes('address', 'string')
+            ->setAllowedTypes('idNumber', ['string', 'int'])
+            ->setAllowedTypes('confirm', ['string', 'int'])
+            ->resolve($client)
+        ;
+    }
+}
