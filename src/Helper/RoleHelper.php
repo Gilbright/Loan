244f28779e6d7cdd23413e144245d8fd5e270305
Marwane Tchassama <marwane.tchassama@enuygun.com>
2021-03-05 03:07:01 +0300

    RoleHelper class added

---
+++ src/Helper/RoleHelper.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PHPstorm.
+ * User: Marwane Tchassama
+ * Date: 5.03.2021
+ * Time: 03:05
+ */
+
+namespace App\Helper;
+
+
+class RoleHelper
+{
+    public const BOSS = 'ROLE_BOSS';
+
+    public const EXPERT = 'ROLE_EXPERT';
+
+    public const ACCOUNTANT = 'ROLE_ACCOUNTANT';
+
+    public const SECRETARY = 'ROLE_SECRETARY';
+}
