7970e11c63e78a85ff7b7f1c1981805dfa9fc664
Marwane Tchassama <marwane.tchassama@enuygun.com>
2021-05-10 10:04:36 +0000

    account page and logout added

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,9 +16,9 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+             return $this->redirectToRoute('admin');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -34,5 +34,15 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/account", name="app_account")
+     */
+    public function account()
+    {
+        return $this->render('pages/account.html.twig',
+            ['user' => $this->getUser()]
+        );
     }
 }
