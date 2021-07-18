abf40bbbc3137974888399ef99b95485e366e6f6
Marwane Tchassama <marwane.tchassama@enuygun.com>
2021-07-18 23:13:12 +0300

    employer added to entity in eventSubscriber

--- src/Service/MailerManager.php
+++ src/Service/MailerManager.php
@@ -18,6 +18,7 @@ use Symfony\Component\Mime\Email;
 
 class MailerManager
 {
+    const FROM_ADDRESS = 'loanphenix@gmail.com';
     /**
      * @var MailerInterface $mailerSender
      */
@@ -31,7 +32,7 @@ class MailerManager
     public function sendMailNotification(Project $project,Employee $employee)
     {
         $mailObject = (new TemplatedEmail())
-            ->from('loanphenix@gmail.com')
+            ->from(self::FROM_ADDRESS)
             ->to($employee->getEmail())
             ->subject('Notification sur le statut du projet ' . $project->getProjectId())
             ->htmlTemplate('mails/employee_mail.html.twig')
--- src/Service/NoteManager.php
+++ src/Service/NoteManager.php
@@ -4,13 +4,10 @@
 namespace App\Service;
 
 
-use App\Entity\Employee;
 use App\Entity\Note;
-use App\Helper\RoleTranslator;
 use App\Repository\NoteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query\Expr\Join;
-use Symfony\Component\Security\Core\Security;
 
 class NoteManager
 {
@@ -25,37 +22,25 @@ class NoteManager
     private $entityManager;
 
     /**
-     * @var Security $security
-     */
-    private $security;
-
-    /**
      * NoteManager constructor.
      * @param EntityManagerInterface $entityManager
      * @param NoteRepository $noteRepository
      */
-    public function __construct(EntityManagerInterface $entityManager, NoteRepository $noteRepository, Security $security)
+    public function __construct(EntityManagerInterface $entityManager, NoteRepository $noteRepository)
     {
         $this->entityManager = $entityManager;
         $this->noteRepository = $noteRepository;
-        $this->security = $security;
     }
 
     public function execute(array $data)
     {
         $content = $data['noteContent'];
-        if ($content) {
-            /** @var Employee $employee */
-            $employee = $this->security->getUser();
-            $authorRole = current(array_filter($employee->getRoles(), function ($role) {
-                return $role !== 'ROLE_USER';
-            }));
 
+        if ($content) {
             $noteEntity = (new Note())
-                ->setAuthorRole(RoleTranslator::translate($authorRole))
                 ->setContent($content)
-                ->setEmployee($employee)
-                ->setProject($data['project']);
+                ->setProject($data['project'])
+            ;
 
             $this->entityManager->persist($noteEntity);
             $this->entityManager->flush();
